f4e909434c5db8c53d114f8c87ffbc179aeb4efa
yoneda@73b1_desktop_22 <yoneda@jsk.imi.i.u-tokyo.ac.jp>
2025-06-20 15:55:57 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# CVATのインストールスクリプト
+
+## install
+```sh
+bach ./install_cvat_wsl_ubuntu24.sh
+```
+
+## cvatを開始
+```sh
+docker compose -f docker-compose.yml -f components/serverless/docker-compose.serverless.yml -f docker-compose.override.yml up -d
+```
+
+ブラウザのurlに`localhost:8080`と打つと立ち上がったアプリにアクセスできる.
+
+最初にアカウントを作るよう要求されるので要求に沿ってアカウントを作成する.
+
+## cvat使い方
+後で書く
+
+## cvatを終了
+```sh
+docker compose -f docker-compose.yml -f components/serverless/docker-compose.serverless.yml -f docker-compose.override.yml down
+```
---
+++ install_cvat_wsl_ubuntu24.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# 最新のパッケージ情報を取得
+sudo apt update && sudo apt upgrade -y
+
+# dockerをインストール
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl enable docker
+
+# cvat をインストール
+git clone https://github.com/cvat-ai/cvat ~/cvat
+cd ~/cvat
+cp ~/cvat_installer/docker-compose.override.yml .
+mkdir -p ~/particle_detection/pictures
+
+echo "install finished!\n"
+echo "run following commands to start up\n docker compose -f docker-compose.yml -f components/serverless/docker-compose.serverless.yml -f docker-compose.override.yml up -d
+"
